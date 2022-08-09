83c0c8b461509b0bceedeec6d3f6f831783b4cbf
Juju <jujuchu@pixnet.tw>
2022-08-09 21:03:16 +0800

    feat: 建立輸入欄位元件

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import * as ReactDOM from 'react-dom/client';
+import App from './components/App'
+
+ReactDOM.createRoot(document.querySelector('#root')).render(<App />);
