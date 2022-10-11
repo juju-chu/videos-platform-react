ec022ec832b7e74065880dbc168394b7245fe2fb
Juju <jujuchu@pixnet.tw>
2022-10-11 19:42:00 +0800

    feat: 更新各影片的key

--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -2,12 +2,14 @@ import React from 'react';
 import VideoItem from './VideoItem';
 
 const VideoList = ({ videos, onVideoSelect }) => {
-    const renderedList = videos.map((video, index) => {
-        return <VideoItem
-                    onVideoSelect={onVideoSelect}
-                    key={index}
-                    video={video}
-                />;
+    const renderedList = videos.map((video) => {
+        return (
+            <VideoItem
+                onVideoSelect={onVideoSelect}
+                key={video.id.videoId}
+                video={video}
+            />
+        );
     });
     return <div className="ui relaxed divided list">{renderedList}</div>
 };
